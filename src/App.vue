b4584443da748726d5d60cb88e8fc342ca48faba
Oliver Pringle <opringle@fb.com>
2023-05-30 11:18:17 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <main>
+    <v-bottom-sheet>
+      <template v-slot:activator="{ props }">
+        <v-btn v-bind="props" text="Click Me"></v-btn>
+      </template>
+
+      <v-card
+        title="Bottom Sheet"
+        text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, eos? Nulla aspernatur odio rem, culpa voluptatibus eius debitis dolorem perspiciatis asperiores sed consectetur praesentium! Delectus et iure maxime eaque exercitationem!"
+      ></v-card>
+    </v-bottom-sheet>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
